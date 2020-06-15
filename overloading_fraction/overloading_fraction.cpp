6f1873767c9ed2c8b6a59f9818599b3e6b8915a9
Bruno Wilkinson <bruno.wilkinson@gmail.com>
2020-06-15 19:01:31 +0100

    Update overloading_fraction.cpp

--- overloading_fraction/overloading_fraction.cpp
+++ overloading_fraction/overloading_fraction.cpp
@@ -30,6 +30,8 @@ public:
     friend Fraction operator*(const Fraction& a, const Fraction& b);
     friend Fraction operator*(int a, const Fraction& b);
     friend Fraction operator*(const Fraction& a, int b);
+    friend std::ostream& operator<< (std::ostream &out, const Fraction &fraction);
+    friend std::istream& operator>> (std::istream& in, Fraction& fraction);
 };
 
 Fraction operator*(const Fraction &f1, const Fraction &f2)
@@ -47,28 +49,53 @@ Fraction operator*(const Fraction &f1, int value)
     return { f1.m_numerator * value, f1.m_denominator };
 }
 
+std::ostream& operator<< (std::ostream &out, const Fraction &fraction)
+{
+    out << fraction.m_numerator << '/' << fraction.m_denominator;
+    return out;
+}
+
+std::istream& operator>> (std::istream& in, Fraction& fraction)
+{
+    in >> fraction.m_numerator;
+    in.ignore(std::numeric_limits<std::streamsize>::max(), '/');
+    in >> fraction.m_denominator;
+    fraction.reduce();
+    return in;
+}
+
 int main()
 {
     Fraction f1{ 2, 5 };
-    f1.print();
+    std::cout << f1 << '\n';
 
     Fraction f2{ 3, 8 };
-    f2.print();
+    std::cout << f2 << '\n';
 
     Fraction f3{ f1 * f2 };
-    f3.print();
+    std::cout << f3 << '\n';
 
     Fraction f4{ f1 * 2 };
-    f4.print();
+    std::cout << f4 << '\n';
 
     Fraction f5{ 2 * f2 };
-    f5.print();
+    std::cout << f5 << '\n';
 
     Fraction f6{ Fraction{1, 2} *Fraction{2, 3} *Fraction{3, 4} };
-    f6.print();
+    std::cout << f6 << '\n';
 
     Fraction f7{ 0, 6 };
-    f7.print();
+    std::cout << f7 << '\n';
+
+    Fraction f8;
+    std::cout << "Enter fraction 1: ";
+    std::cin >> f8;
+
+    Fraction f9;
+    std::cout << "Enter fraction 2: ";
+    std::cin >> f9;
+
+    std::cout << f8 << " * " << f9 << " is " << f8 * f9 << '\n'; // note: The result of f1 * f2 is an r-value
 
     return 0;
 }
