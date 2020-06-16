4fc61a50aa5ef78250d536507a515bdb9f856f22
Bruno Wilkinson <bruno.wilkinson@gmail.com>
2020-06-16 09:49:59 +0100

    update

---
+++ cars_overloading/cars_overloading.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Car
+{
+private:
+    std::string m_make;
+    std::string m_model;
+public:
+    Car(std::string make, std::string model) : 
+        m_make{ make }, m_model { model }
+    {}
+    friend bool operator==(const Car& c1, const Car& c2);
+    friend bool operator!=(const Car& c1, const Car& c2);
+    friend std::ostream& operator<< (std::ostream& out, const Car& car);
+    friend bool operator<(const Car& c1, const Car& c2)
+    {
+        if (c1.m_make == c2.m_make)
+            return c1.m_model < c2.m_model;
+        return c1.m_make < c2.m_make;
+    }
+};
+bool operator==(const Car& c1, const Car& c2)
+{
+    return (c1.m_make == c2.m_make && c1.m_model == c2.m_model);
+}
+
+bool operator!=(const Car& c1, const Car& c2)
+{
+    return !(c1 == c2);
+}
+
+std::ostream& operator<< (std::ostream &out, const Car &car)
+{
+    out << '(' << car.m_make << ", " << car.m_model << ')';
+    return out;
+}
+
+int main()
+{
+    std::vector<Car> cars{
+      { "Toyota", "Corolla" },
+      { "Honda", "Accord" },
+      { "Toyota", "Camry" },
+      { "Honda", "Civic" }
+    };
+
+    std::sort(cars.begin(), cars.end()); // requires an overloaded operator<
+
+    for (const auto& car : cars)
+        std::cout << car << '\n'; // requires an overloaded operator<<
+
+    return 0;
+}
