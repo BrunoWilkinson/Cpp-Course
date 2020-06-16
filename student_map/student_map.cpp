36fd906890382532c826a09850c617a0ff4273e1
Bruno Wilkinson <bruno.wilkinson@gmail.com>
2020-06-16 15:45:06 +0100

    Create student_map.cpp

---
+++ student_map/student_map.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+struct StudentGrade
+{
+	std::string name{};
+	char grade;
+};
+
+class GradeMap
+{
+private:
+	std::vector<StudentGrade> m_map{};
+public:
+	char& operator[] (const std::string& name);
+};
+
+char& GradeMap::operator[] (const std::string& name)
+{
+	auto found{ std::find_if(m_map.begin(), m_map.end(), [&](const StudentGrade &student) {
+		return (student.name == name);
+		}) };
+	if (found != m_map.end())
+		return found->grade;
+	m_map.push_back({ name });
+	return m_map.back().grade;
+}
+
+int main()
+{
+	GradeMap grades{};
+
+	grades["Joe"] = 'A';
+	grades["Frank"] = 'B';
+
+	std::cout << "Joe has a grade of " << grades["Joe"] << '\n';
+	std::cout << "Frank has a grade of " << grades["Frank"] << '\n';
+
+	return 0;
+}
